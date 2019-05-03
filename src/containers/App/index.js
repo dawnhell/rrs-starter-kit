9e737c062f6b0ed77a7438b1336ece90aaa592fa
Uladzislau Klachkou <uladzislau.klachkou@altoros.com>
2019-05-03 15:18:56 +0300

    Add Redux/Saga store.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import Header from 'src/containers/Header';
 import Footer from 'src/containers/Footer';
 import ErrorBoundaryContainer from 'src/containers/ErrorBoundary';
 
 import Router from 'src/routes/router';
+import store from 'src/store';
 
 class App extends React.Component {
   render () {
     return (
-      <div>
+      <Provider store={store}>
         <Header />
 
         <ErrorBoundaryContainer>
@@ -17,7 +19,7 @@ class App extends React.Component {
         </ErrorBoundaryContainer>
 
         <Footer />
-      </div>
+      </Provider>
     )
   }
 }
